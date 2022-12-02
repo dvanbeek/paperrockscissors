94b4930203fe2f04a3e65b85bdeeac64440ae201
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 11:45:11 +0100

    Remove unused imports

--- src/main/java/org/ysbijl/PaperRockScissors.java
+++ src/main/java/org/ysbijl/PaperRockScissors.java
@@ -1,8 +1,5 @@
 package org.ysbijl;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class PaperRockScissors {
     public static void main(String[] args) {
         final String[] choices = new String[] {"rock", "paper", "scissors"};
