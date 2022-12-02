b47f2df7c7b6ab899fd87b91fdb3b711d7b82706
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 10:38:47 +0100

    scissor -> scissors to pass tests

--- src/main/java/org/ysbijl/Game.java
+++ src/main/java/org/ysbijl/Game.java
@@ -11,8 +11,8 @@ public class Game {
     public String scoreGame(String userMove, String compMove) {
         if (userMove.equals(compMove)){
             return "tie";
-        } else if ((userMove.equals("rock") && compMove.equals("scissor")) ||
-                   (userMove.equals("scissor") && compMove.equals("paper")) ||
+        } else if ((userMove.equals("rock") && compMove.equals("scissors")) ||
+                   (userMove.equals("scissors") && compMove.equals("paper")) ||
                    (userMove.equals("paper") && compMove.equals("rock"))) { // Winning cases from user perspective
             return "win";
         }
