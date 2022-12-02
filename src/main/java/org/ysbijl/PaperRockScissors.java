36c7a6f6ccd415479d684b79233d6389a83ddb57
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 10:45:19 +0100

    Created Player class Use Player class for user and computer in PaperRockScissors

--- src/main/java/org/ysbijl/PaperRockScissors.java
+++ src/main/java/org/ysbijl/PaperRockScissors.java
@@ -2,11 +2,14 @@ package org.ysbijl;
 
 public class PaperRockScissors {
     public static void main(String[] args) {
-        String user = "rock";
-        String comp = "scissors";
+        Player user = new Player();
+        Player comp = new Player();
+
+        user.setMove("rock");
+        comp.setMove("paper");
 
         Game singleGame = new Game();
-        singleGame.scoreGame(user, comp);
+        singleGame.scoreGame(user.getMove(), comp.getMove());
     }
 
 
