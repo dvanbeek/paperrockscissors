a6144a42ca894e55dfdf5c997eef66c7a5b9d25f
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 10:18:52 +0100

    Create method for scoring of a game of paper-rock-scissors Created test for method scoreGame Updated pom.xml with junit5

--- src/main/java/org/ysbijl/PaperRockScissors.java
+++ src/main/java/org/ysbijl/PaperRockScissors.java
@@ -4,4 +4,8 @@ public class PaperRockScissors {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void scoreGame() {
+
+    }
 }
---
+++ src/test/java/org/ysbijl/PaperRockScissorsTest.java
@@ -0,0 +1,10 @@
+package org.ysbijl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaperRockScissorsTest {
+
+    @org.junit.jupiter.api.Test
+    void scoreGame() {
+    }
+}
