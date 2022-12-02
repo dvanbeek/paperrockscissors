1ceebcdabf65240d9f670440a37f6baf36c8096b
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 11:29:32 +0100

    Put game in loop Make method asking user to keep playing. If not, loop will be broken.

--- src/main/java/org/ysbijl/PaperRockScissors.java
+++ src/main/java/org/ysbijl/PaperRockScissors.java
@@ -1,18 +1,35 @@
 package org.ysbijl;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PaperRockScissors {
     public static void main(String[] args) {
         final String[] choices = new String[] {"rock", "paper", "scissors"};
+        final String[] playing = new String[] {"yes", "no"};
 
+        boolean keepPlaying = true;
         UPC user = new UPC();
         NPC comp = new NPC();
 
-        user.setMove(user.askUserMove(choices));
-        comp.setMove(comp.selectRandomMove(choices));
+        while (keepPlaying) {
+            user.setMove(user.askUserMove(choices));
+            comp.setMove(comp.selectRandomMove(choices));
 
-        Game singleGame = new Game();
-        String state = singleGame.scoreGame(user.getMove(), comp.getMove());
+            Game singleGame = new Game();
+            String state = singleGame.scoreGame(user.getMove(), comp.getMove());
+
+            keepPlaying = askUserToKeepPlaying(playing);
+        }
     }
 
-
+    private static boolean askUserToKeepPlaying(String[] options) {
+        Scanner userInput = new Scanner(System.in);
+        String userChoice = "";
+        while (!(Arrays.asList(options).contains(userChoice))) {
+            System.out.println("Will you continue playing? (yes/no)");
+            userChoice = userInput.nextLine();
+        }
+        return userChoice.equals("yes");
+    }
 }
