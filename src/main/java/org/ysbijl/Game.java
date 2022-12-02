3e95af6b876e07bbe974889af068c479b2fa8999
Yannick Bijl <yannick.bijl@gmail.com>
2022-12-02 11:41:23 +0100

    Remove unnecessary code

--- src/main/java/org/ysbijl/Game.java
+++ src/main/java/org/ysbijl/Game.java
@@ -1,12 +1,6 @@
 package org.ysbijl;
 
 public class Game {
-    String user, comp;
-
-    public void Game(String user, String comp) {
-        this.user = user;
-        this.comp = comp;
-    }
 
     public String scoreGame(String userMove, String compMove) {
         if (userMove.equals(compMove)){
