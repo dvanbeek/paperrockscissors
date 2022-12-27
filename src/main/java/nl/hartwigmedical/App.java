7b1afdd756d46ea38c737c71e3d6f4e6dafcc455
martijnschouwe <martijnschouwe@thesting.com>
2022-12-27 18:35:11 +0100

    bootstrap app

--- src/main/java/nl/hartwigmedical/App.java
+++ src/main/java/nl/hartwigmedical/App.java
@@ -1,12 +1,17 @@
 package nl.hartwigmedical;
 
+import nl.hartwigmedical.game.Game;
+
+import java.util.Scanner;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Starting the game....hold on..");
-
-        //start init of the game
-
+        Game game = Game.initGame();
+        game.askForHumanPlayerName(scanner);
+        game.nextRound(scanner);
     }
 
 }
