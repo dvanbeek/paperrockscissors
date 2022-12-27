e8c72cc3f0c4d294be3f31f1d38094cce96df885
martijnschouwe <martijnschouwe@thesting.com>
2022-12-27 11:37:01 +0100

    Add bootstrap class and unit test setup

---
+++ src/main/java/nl/hartwigmedical/App.java
@@ -0,0 +1,9 @@
+package nl.hartwigmedical;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("The start of the game.");
+    }
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AppTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+
+    @Test
+    public void testGameBootstrap() {
+
+        fail("Not implemented yet");
+
+    }
+
+}
