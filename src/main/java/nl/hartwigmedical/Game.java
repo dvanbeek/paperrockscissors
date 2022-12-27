fd6415dfbd2cbd48c90767654dfb3422cdf48a46
martijnschouwe <martijnschouwe@thesting.com>
2022-12-27 13:03:32 +0100

    Add computer player to game

--- src/main/java/nl/hartwigmedical/Game.java
+++ src/main/java/nl/hartwigmedical/Game.java
@@ -1,13 +1,29 @@
 package nl.hartwigmedical;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     static Game game ;
-    public static Game initGame(){
+    //only 2 players allowed
+    private List players = new ArrayList();
+    public static Game initGame() throws TooManyPLayersException {
         if (game == null){
             game = new Game();
+            game.addPlayer(new ComputerPlayer());
         }
         return game;
     }
 
+    public void addPlayer(Player player) throws TooManyPLayersException {
+        if (this.players.size() > 1){
+            throw new TooManyPLayersException();
+        }
+        this.players.add(player);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,14 +1,38 @@
+import lombok.SneakyThrows;
 import nl.hartwigmedical.Game;
+import nl.hartwigmedical.HumanPlayer;
+import nl.hartwigmedical.TooManyPLayersException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class GameTest {
 
     Game game;
+
     @Test
+    @SneakyThrows
     public void testInitGame(){
         game = Game.initGame();
         Assertions.assertNotNull(game);
     }
+
+
+    public void  testAskForPlayerName() {
+        fail("Not implemenetd");
+    }
+
+    @Test
+    @SneakyThrows
+    public void testShouldThrowExceptionIfTooManyPlayer() {
+        game = Game.initGame();
+        Assertions.assertThrows(TooManyPLayersException.class, ()-> {
+            game.addPlayer(new HumanPlayer("Martijn"));
+            game.addPlayer(new HumanPlayer("ThreeIsACrowd"));
+        });
+    }
+
 
 }
