07fa7f170e841e4015379469376530c037b37b3e
David Puroja <dpuroja@outlook.com>
2023-04-09 16:49:47 +0200

    Add general enumerators

---
+++ src/main/java/games/GameInputOptions.java
@@ -0,0 +1,6 @@
+package src.main.java.games;
+
+public enum GameInputOptions implements IChoice {
+    PLAY,
+    STOP;
+}
---
+++ src/main/java/games/ResultsEnum.java
@@ -0,0 +1,8 @@
+package src.main.java.games;
+
+public enum ResultsEnum {
+    WIN,
+    TIE,
+    LOSS,
+    UNDECIDED
+}
