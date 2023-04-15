eab4f12c09c712ab8afd9c143e5716049667759f
Kasper Wolsink <kwolsink@gmail.com>
2023-04-15 11:53:51 +0200

    Implement game class

---
+++ app/src/main/java/paperrockscissors/Game.java
@@ -0,0 +1,40 @@
+package paperrockscissors;
+
+import paperrockscissors.players.Player;
+import paperrockscissors.rules.Rules;
+
+public class Game {
+
+    private final Rules rules;
+    private final Player player1;
+    private final Player player2;
+    private final CLI cli;
+
+    private int[] scoreboard = new int[3];
+
+    public Game(CLI cli, Rules rules, Player player1, Player player2) {
+        this.cli = cli;
+        this.rules = rules;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void nextRound() {
+        cli.announceRoundStart();
+        Move move1 = player1.makeMove();
+        Move move2 = player2.makeMove();
+        int winnerI = rules.decideWinner(move1, move2);
+        scoreboard[winnerI]++;
+        String winnerString = winnerI == 0 ? null : (winnerI == 1 ? player1.getName() : player2.getName());
+        cli.summarizeRound(player1, player2, move1, move2, winnerString);
+        if (cli.promptPlayAnotherRound()) {
+            nextRound();
+        } else {
+            endGame();
+        }
+    }
+
+    public void endGame() {
+        cli.summarizeGame(player1, player2, scoreboard[1], scoreboard[2], scoreboard[0]);
+    }
+}
