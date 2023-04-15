d9e92171a7ea2510fcd9671b0fba3757b18cbcf5
Kasper Wolsink <kwolsink@gmail.com>
2023-04-15 11:53:36 +0200

    add fromString method

--- app/src/main/java/paperrockscissors/Move.java
+++ app/src/main/java/paperrockscissors/Move.java
@@ -10,4 +10,13 @@ public enum Move {
     public static Set<Move> getAllMoves() {
         return Set.of(ROCK, PAPER, SCISSORS);
     }
+
+    public static Move fromString(String moveString) {
+        return switch (moveString.toLowerCase().trim()) {
+            case "rock" -> ROCK;
+            case "paper" -> PAPER;
+            case "scissors" -> SCISSORS;
+            default -> null;
+        };
+    }
 }
