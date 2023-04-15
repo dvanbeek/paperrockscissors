9bf02db16ff9948a4a22e1ce418c556f2ebc091f
Kasper Wolsink <kwolsink@gmail.com>
2023-04-15 11:54:37 +0200

    Implement app.java such that it initialises a game

--- app/src/main/java/paperrockscissors/App.java
+++ app/src/main/java/paperrockscissors/App.java
@@ -3,12 +3,20 @@
  */
 package paperrockscissors;
 
+import paperrockscissors.players.ComputerPlayer;
+import paperrockscissors.players.HumanPlayer;
+import paperrockscissors.players.Player;
+import paperrockscissors.players.strategies.RandomStrategy;
+import paperrockscissors.rules.DefaultRules;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        CLI cli = new CLI();
+        String humanName = cli.promptName();
+        Player humanPlayer = new HumanPlayer(humanName, cli);
+        Player computerPlayer = new ComputerPlayer("Computron", new RandomStrategy());
+        Game defaultGame = new Game(cli, new DefaultRules(), humanPlayer, computerPlayer);
+        defaultGame.nextRound();
     }
 }
