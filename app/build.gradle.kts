8c03afaeec631d4b53bdd1a3f8f78e3134a57d46
Kasper Wolsink <kwolsink@gmail.com>
2023-04-15 12:14:27 +0200

    install mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,8 @@ repositories {
 dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-
+    // Use mockito for mocks
+    testImplementation("org.mockito:mockito-core:3.+");
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
 }
