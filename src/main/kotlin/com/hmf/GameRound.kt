e7a07b6005b429dd013c802248a2e6fddbe5e87f
Vlad Sergeev <vlad@lavalgroup.io>
2023-09-25 12:03:56 +0200

    unit-test for game rules

--- src/main/kotlin/com/hmf/GameRound.kt
+++ src/main/kotlin/com/hmf/GameRound.kt
@@ -1,7 +1,14 @@
 package com.hmf
 
 class GameRound {
+
+    private val rules: Array<Array<Outcome>> = arrayOf(
+        arrayOf(Outcome.DRAW,        Outcome.PLAYER_LOST, Outcome.PLAYER_WON), // we can also use shortArrayOf or doubleArrayOf...
+        arrayOf(Outcome.PLAYER_WON,  Outcome.DRAW,        Outcome.PLAYER_LOST),
+        arrayOf(Outcome.PLAYER_LOST, Outcome.PLAYER_WON,  Outcome.DRAW)
+    )
+
     fun getOutcome(player: Element, opponent: Element): Outcome {
-        return Outcome.DRAW
+        return rules[player.idx][opponent.idx]
     }
 }
--- src/test/kotlin/com/hmf/GameRoundTests.kt
+++ src/test/kotlin/com/hmf/GameRoundTests.kt
@@ -7,10 +7,14 @@ class GameRoundTests {
 
     @Test
     fun TestSingleRound(){
-
         val round = GameRound()
-        val outcome0 = round.getOutcome(Element.ROCK, Element.ROCK)
+        val outcome0 = round.getOutcome(player=Element.ROCK, opponent=Element.ROCK)
         assertEquals(outcome0, Outcome.DRAW)
 
+        val outcome1 = round.getOutcome(player=Element.ROCK, opponent=Element.PAPER)
+        assertEquals(outcome1, Outcome.PLAYER_LOST)
+
+        val outcome2 = round.getOutcome(player=Element.ROCK, opponent=Element.SCISSORS)
+        assertEquals(outcome2, Outcome.PLAYER_WON)
     }
 }
